b91fd6b710844f78828fada755af80e4bb0cca32
Markss07 <markinfotech07@gmail.com>
2019-04-27 17:54:48 +0530

    Made Some Changes 1. Added Click Event In MainActivity

--- KotlinWithORMLite/app/src/main/java/com/example/kotlinwithormlite/MainActivity.kt
+++ KotlinWithORMLite/app/src/main/java/com/example/kotlinwithormlite/MainActivity.kt
@@ -4,9 +4,11 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
+import com.example.kotlinwithormlite.DBAdapter.ORMDataBase
 import com.example.kotlinwithormlite.DBAdapter.Tables.TestTable
 import com.example.kotlinwithormlite.DBAdapter.Tables.TestingTable
 import kotlinx.android.synthetic.main.activity_main.*
+import java.lang.StringBuilder
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
 
@@ -29,6 +31,28 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
                 }
                 Toast.makeText(this,"Create Table Success", Toast.LENGTH_SHORT).show()
             }
+            btn_remove -> {
+                dao.removeAll()
+                Toast.makeText(this, "remove success", Toast.LENGTH_SHORT).show()
+            }
+            btn_query -> {
+                val tableAll = dao.queryForAll()
+                if (tableAll.size == 0){
+                    Toast.makeText(this, "remove success", Toast.LENGTH_SHORT).show()
+                    return
+                }
+                val stringBuilder = StringBuilder()
+                for (table in tableAll){
+                    stringBuilder.append(table.toString())
+                    stringBuilder.append("\n")
+                }
+                Toast.makeText(this, "remove success", Toast.LENGTH_SHORT).show()
+            }
         }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        ORMDataBase.close()
     }
 }
